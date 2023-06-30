fb35e31760062cd7c885988a3e6335635b1185f6
Elena <pyatochkaa@mail.ru>
2023-06-30 16:50:40 +0300

    commit

---
+++ src/main/java/task4.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/*
+ Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
+и возвращающий новый массив, каждый элемент которого равен частному элементов двух входящих массивов в той же ячейке.
+Если длины массивов не равны, необходимо как-то оповестить пользователя.
+Важно: При выполнении метода единственное исключение, которое пользователь может увидеть - RuntimeException, т.е. ваше.
+*/
+public class task4 {
+    public static void main(String[] args) {
+        int[] arr1 = {9, 8, 7, 6};
+        int[] arr2 = {1, 2, 3, 4};
+
+        createArr(arr1, arr2);
+    }
+
+    public static void createArr(int[] arr1, int[] arr2) {
+        float[] arr3 = new float[arr1.length];
+        if (arr1.length == arr2.length) {
+            for (int i = 0; i < arr2.length; i++) {
+                if (arr2[i] == 0) {
+                    throw new RuntimeException("делить на ноль нельзя");
+                }
+                arr3[i] = (float) arr1[i] / (float) arr2[i];
+            }
+            System.out.println(Arrays.toString(arr3));
+
+        } else {
+            throw new RuntimeException("длины массивов не равны");
+        }
+
+    }
+}
